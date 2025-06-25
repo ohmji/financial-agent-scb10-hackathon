4d752ab8305313c4f3ce5f088a40b8c11abe8a27
ohmji <53232225+ohmji@users.noreply.github.com>
2025-06-25 23:20:19 +0700

    optimize parameter

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ load_dotenv()
 
 
 # HuggingFace model setup
-hf_model_id = "Konthee/Llama-3.1-8B-ThaiInstruct"
+hf_model_id = "Surasan/qwen2.5-7b-financial-thai-4bit-full"
 
 MAX_NEW_TOKENS = 1
 TEMPERATURE = 0.1
