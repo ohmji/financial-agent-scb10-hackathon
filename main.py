1f7473ec510c832eedf4103250a48211722ac43a
ohmji <53232225+ohmji@users.noreply.github.com>
2025-06-26 00:50:13 +0700

    optimize parameter

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ load_dotenv()
 
 
 # HuggingFace model setup
-hf_model_id = "Naphon/Phi-4-thai-financial-instruct"
+hf_model_id = "Surasan/qwen3-14b-unsloth-bnb-qa-lora_full_3000"
 
-MAX_NEW_TOKENS = 256
+MAX_NEW_TOKENS = 50
 TEMPERATURE = 0.1
 SLEEP_TIME = 1.5
 
